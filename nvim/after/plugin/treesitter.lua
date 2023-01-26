59b4e13c7bfbb93ea3462983a6866100c729489b
Fluffy Kalashnikov <ivar.sidorsson@gmail.com>
2023-01-26 21:58:33 +0100

    Home sync

---
+++ nvim/after/plugin/treesitter.lua
@@ -0,0 +1,15 @@
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = { 
+    "help", 
+    "c", 
+    "lua", 
+    "vim", 
+    "help" 
+  },
+  sync_install = false,
+  auto_install = true,
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+}
